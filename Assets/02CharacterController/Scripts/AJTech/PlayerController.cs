82c72c8c3459f339c9cb39fbd06d2cd3cfd73fb8
DESKTOP-P23E8J4\Daddy <devnetdot@gmail.com>
2018-07-10 21:34:37 +0300

    Added Body rotation according to WSAD keys

--- Assets/02CharacterController/Scripts/AJTech/PlayerController.cs
+++ Assets/02CharacterController/Scripts/AJTech/PlayerController.cs
@@ -1,16 +1,54 @@
-﻿using System.Collections;
+﻿#define TestMoveRays
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+     [System.Serializable]
+    public struct RayCastObjects
+    {
+         [SerializeField]
+         internal GameObject sphMiddle;
+         [SerializeField]
+        internal GameObject sphFront;
+         [SerializeField]
+        internal GameObject sphBack;
+         [SerializeField]
+        internal GameObject sphRight;
+         [SerializeField]
+        internal GameObject sphLeft;
+         [SerializeField]
+        internal GameObject sphMiddleCast;
+         [SerializeField]
+        internal GameObject sphFrontCast;
+         [SerializeField]
+        internal GameObject sphBackCast;
+         [SerializeField]
+        internal GameObject sphRightCast;
+         [SerializeField]
+        internal GameObject sphLeftCast;
+    }
 
-    [Header("Gizmos")]
-    public GameObject sphere;
-    public GameObject sphere2;
-    public GameObject sphere3;
-    public GameObject sphere4;
+     [System.Serializable]
+    public class RayCastOffsets
+     {
+         [SerializeField]
+         internal float Mdl = 4.85f;
+         [SerializeField]
+         internal float Fwd = 1.5f;
+         [SerializeField]
+         internal float Bckd = 1.7f;
+         [SerializeField]
+         internal float Right = 1.5f;
+         [SerializeField]
+         internal float Left = 1.5f;
+     }
 
+    [SerializeField]
+    private RayCastObjects rayCasts = new RayCastObjects();
+    [SerializeField]
+    private RayCastOffsets rayOffsets = new RayCastOffsets();
 
     public float movementSpeed;
     public float gravity = -2.5f;
@@ -20,36 +58,43 @@ public class PlayerController : MonoBehaviour
     private Vector3 velocity;
     private Vector3 move;
 
+    private RobotInput input;
+
     private bool grounded;
     private float currentGravity = 0;
     private CapsuleCollider playerCollider;
+    private Rigidbody rb;
+    private bool isInAir;
+    private float previousFrameHitY;
+    private string lastMoveState;
     // Use this for initialization
     void Start()
     {
+        input = this.GetComponent<RobotInput>();
        // playerCollider = this.GetComponent<CapsuleCollider>();
        // length = playerCollider.radius * 2;
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         SimpleMove();
-
         FinalMove();
         SlopeChecking();
     }
    
     void SimpleMove()
     {
-        move = new Vector3(Input.GetAxis("Horizontal"), currentGravity, Input.GetAxis("Vertical"));
+        move = new Vector3(input.Horizontal, currentGravity, input.Vertical);
     }
 
     void FinalMove()
     {
         velocity = move * movementSpeed;
         velocity = transform.TransformDirection(velocity);
-        this.transform.position += velocity * Time.deltaTime;
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        //this.transform.position += velocity * Time.deltaTime;
     }
 
     void ApplyGravity()
@@ -64,31 +109,91 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void DrawRayGizmos(Probe rayMiddle,Probe rayForward,Probe rayBack,Probe rayRight,Probe rayLeft)
+    {
+        rayCasts.sphMiddle.transform.position = rayMiddle.hitRay.origin;
+        rayCasts.sphFront.transform.position = rayForward.hitRay.origin;
+        rayCasts.sphBack.transform.position = rayBack.hitRay.origin;
+        rayCasts.sphRight.transform.position = rayRight.hitRay.origin;
+        rayCasts.sphLeft.transform.position = rayLeft.hitRay.origin;
+
+        rayCasts.sphMiddleCast.transform.position = rayMiddle.hitResult.point;
+        rayCasts.sphFrontCast.transform.position = rayForward.hitResult.point;
+        rayCasts.sphBackCast.transform.position = rayBack.hitResult.point;
+        rayCasts.sphRightCast.transform.position = rayRight.hitResult.point;
+        rayCasts.sphLeftCast.transform.position = rayLeft.hitResult.point;
+    }
+
     void SlopeChecking()
     {
-        Ray rayForward = new Ray(transform.position + transform.forward *1.88f, Vector3.down);
-        Ray rayBack = new Ray(transform.position - transform.forward * 2.82f, Vector3.down);
+        Vector3 downOffset = transform.position - transform.up * rayOffsets.Mdl;
 
-        sphere.transform.position = rayForward.origin;
-        sphere2.transform.position = rayBack.origin;
+        Probe rayMiddle = MakeRaycast(downOffset);
+        Probe rayForward = MakeRaycast(downOffset + transform.forward * rayOffsets.Fwd);
+        Probe rayBack = MakeRaycast(downOffset - transform.forward * rayOffsets.Bckd);
+        Probe rayRight = MakeRaycast(downOffset + transform.right * rayOffsets.Right);
+        Probe rayLeft = MakeRaycast(downOffset  - transform.right * rayOffsets.Left);
 
-        RaycastHit hit1;
-        RaycastHit hit2;
-        Physics.Raycast(rayForward, out hit1,  15f);
-        Physics.Raycast(rayBack, out hit2, 15f);
-
-        sphere3.transform.position = hit1.point;
-        sphere4.transform.position = hit2.point;
-
-        if (hit1.point.y > hit2.point.y)
+        #if TestMoveRays
+        DrawRayGizmos(rayMiddle, rayForward, rayBack, rayRight, rayLeft);
+        #endif
+      
+        isInAir = rayMiddle.hitResult.distance > 0.2f;
+        if (previousFrameHitY - rayMiddle.hitResult.point.y <= 0.2)
         {
+            rb.MovePosition(new Vector3(rb.position.x, rayMiddle.hitResult.point.y + 5f, rb.position.z));
+            previousFrameHitY = rayMiddle.hitResult.point.y;
+            LogState("Moving");
         }
         else
         {
-
+            if (rayMiddle.hitResult.distance > 0.2 && rayForward.hitResult.distance > 0.2
+                && rayBack.hitResult.distance > 0.2
+                && rayRight.hitResult.distance > 0.2
+                && rayLeft.hitResult.distance > 0.2)
+            {
+                Landing();
+                previousFrameHitY -= 10 * Time.fixedDeltaTime;
+                LogState("Falling");
+            }
+            else
+            {
+                LogState("About to fall");
+            }
         }
-        RaycastHit hit = hit1.point.y > hit2.point.y ? hit1 : hit2;
-        this.transform.position = new Vector3(this.transform.position.x, hit.point.y + 5f, this.transform.position.z);
+     
+       // this.transform.position = new Vector3(this.transform.position.x, hit.point.y + 5f, this.tranSsform.position.z);
     }
+
+    private void LogState(string state)
+    {
+        if (lastMoveState != state)
+        {
+            Debug.Log(state);
+            Debug.Log(previousFrameHitY);
+            lastMoveState = state;
+        }
+    }
+    private void Landing()
+    {
+        float f =rb.position.y - 10 * Time.fixedDeltaTime;
+        rb.MovePosition(new Vector3(rb.position.x, f, rb.position.z));
+    }
+
+    private Probe MakeRaycast(Vector3 origin){
+        Probe p = new Probe();
+        p.hitRay = new Ray(origin,Vector3.down);
+        Physics.Raycast(p.hitRay, out p.hitResult, 100f);
+        return p;
+    }
+
+}
+
+public struct Probe 
+{
+
+    public Ray hitRay;
+
+    public RaycastHit hitResult;
 
 }
--- Assets/02CharacterController/Scripts/AJTech/TCameraController.cs
+++ Assets/02CharacterController/Scripts/AJTech/TCameraController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class TCameraController : MonoBehaviour
 {
+
+    public float topRotY = 0;
+
     public bool lockCursor;
     public float mouseSensitivity = 2;
     public Transform robotTop;
     public Transform robotBody;
     public Transform robot;
     public float distFromTarget = 12f;
-    public float distAboveTarget =10f;
+    public float distAboveTarget = 10f;
     public Vector2 pitchMinMax = new Vector2(15, 20);
     public Vector3 camCurrentRotation = Vector3.zero;
     public Vector3 camTargetRotation;
@@ -21,12 +24,16 @@ public class TCameraController : MonoBehaviour
     private float topRotationDamping = 1f;
     public float bodyRotationDamping = 0.2f;
 
+    private RobotInput input;
+
     private float yaw;
     private float pitch;
 
     // Use this for initialization
     void Start()
     {
+        input = this.GetComponent<RobotInput>();
+
         if (lockCursor)
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -37,58 +44,123 @@ public class TCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         camTargetRotation = this.TakeInputRotation();
-        this.transform.eulerAngles = SmoothCamRotation(camTargetRotation);
-        this.robotTop.eulerAngles = SmoothTopRotation(camCurrentRotation);
-        this.robotBody.eulerAngles = SmoothBodyRotation(this.robotTop.eulerAngles);
+        this.transform.eulerAngles = RotateCam(camTargetRotation);
+        Vector3 tempCamCurrentRotation = RotateTop(camCurrentRotation);
+       
+        this.robotTop.eulerAngles = new Vector3(tempCamCurrentRotation.x, tempCamCurrentRotation.y, tempCamCurrentRotation.z + 270);
+        RotateRobot();
+        topRotY = this.robotTop.eulerAngles.y;
 
-        //remember top and body rotation 
-        topCurrentRotation = this.robotTop.eulerAngles;
-        bodyCurrentRotation = this.robotBody.eulerAngles;
-
-        //apply rotation to parent (so as its Forward pointed correctly)
-        robot.eulerAngles = this.robotBody.eulerAngles;
-
-        //negate extra rotation of children caused by parent's rotation 
-        this.robotTop.eulerAngles = topCurrentRotation;
-        this.robotBody.eulerAngles = bodyCurrentRotation;
-
+        //this.robotBody.eulerAngles = SmoothBodyRotation(this.robotTop.eulerAngles);
     }
     void LateUpdate()
     {
-        this.transform.eulerAngles = SmoothCamRotation(camTargetRotation);
-        this.ShiftCamBehind();
+        // this.transform.eulerAngles = SmoothCamRotation(camTargetRotation);
+
+        this.ShiftCamBehindTop();
         topPreviousRotation = this.robotTop.rotation;
     }
     Vector3 TakeInputRotation()
     {
-        yaw +=  Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        yaw += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
         pitch -= Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
         pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
         return new Vector3(pitch, yaw, 0);
     }
-    Vector3 SmoothCamRotation(Vector3 targetRotation)
+
+    void RotateRobot()
     {
-        camCurrentRotation.y = Mathf.LerpAngle(camCurrentRotation.y, targetRotation.y, camRotationDamping *Time.deltaTime);
-        camCurrentRotation.x = Mathf.LerpAngle(camCurrentRotation.x, targetRotation.x, camRotationDamping * Time.deltaTime); 
+        //forward
+        if (input.Vertical > 0 && input.Horizontal == 0)
+        {
+            RotateAllBodyParts(0, 0);
+        }
+        //left
+        if (input.Horizontal < 0 && input.Vertical == 0)
+        {
+            RotateAllBodyParts(-90, 90);
+        }
+        //right
+        if (input.Horizontal > 0 && input.Vertical == 0)
+        {
+            RotateAllBodyParts(90, -90);
+        }
+
+        //backward
+        if (input.Vertical < 0 && input.Horizontal == 0)
+        {
+            RotateAllBodyParts(-180, 180);
+        }
+        //forward-left
+        if (input.Vertical > 0 && input.Horizontal < 0)
+        {
+            float angle = Vector3.Angle(new Vector3(-1, 0, 0).normalized, new Vector3(input.Horizontal, 0, input.Vertical).normalized);
+            RotateAllBodyParts(-angle, angle);
+        }
+        //forward-right
+        if (input.Vertical > 0 && input.Horizontal > 0)
+        {
+            float angle = Vector3.Angle(new Vector3(1, 0, 0).normalized, new Vector3(input.Horizontal, 0, input.Vertical).normalized);
+            RotateAllBodyParts(angle, -angle);
+        }
+        //backward-left
+        if (input.Vertical < 0 && input.Horizontal < 0)
+        {
+            float angle = Vector3.Angle(new Vector3(0, 0, 1).normalized, new Vector3(input.Horizontal, 0, input.Vertical).normalized);
+            RotateAllBodyParts(-angle, angle);
+        }
+        //backward-right
+        if (input.Vertical < 0 && input.Horizontal > 0)
+        {
+            float angle = Vector3.Angle(new Vector3(0, 0, 1).normalized, new Vector3(input.Horizontal, 0, input.Vertical).normalized);
+            RotateAllBodyParts(angle, -angle);
+        }
+    }
+
+    void RotateAllBodyParts(float bodyTrailBehindDegree, float bodyRotationDegree)
+    {
+        topCurrentRotation = this.robotTop.eulerAngles;
+
+        this.robotBody.eulerAngles = RotateBodyAccordingToTop(bodyTrailBehindDegree);
+
+        this.robotTop.eulerAngles = topCurrentRotation;
+
+        //remember top and body rotation  
+        topCurrentRotation = this.robotTop.eulerAngles;
+        bodyCurrentRotation = this.robotBody.eulerAngles;
+
+        //apply rotation to parent (so as its Forward pointed correctly)
+        robot.eulerAngles = new Vector3(this.robotBody.eulerAngles.x, this.robotBody.eulerAngles.y + bodyRotationDegree, this.robotBody.eulerAngles.z);
+
+        //negate extra rotation of children caused by parent's rotation 
+        this.robotBody.eulerAngles = bodyCurrentRotation;
+        this.robotTop.eulerAngles = topCurrentRotation;
+    }
+
+    Vector3 RotateCam(Vector3 targetRotation)
+    {
+        camCurrentRotation.y = Mathf.LerpAngle(camCurrentRotation.y, targetRotation.y, camRotationDamping * Time.deltaTime);
+        camCurrentRotation.x = Mathf.LerpAngle(camCurrentRotation.x, targetRotation.x, camRotationDamping * Time.deltaTime);
         return camCurrentRotation;
     }
 
-    Vector3 SmoothTopRotation(Vector3 targetRotation)
+    Vector3 RotateTop(Vector3 targetRotation)
     {
         return new Vector3(0, Mathf.LerpAngle(robotTop.eulerAngles.y, camCurrentRotation.y, topRotationDamping * (1 - Mathf.Exp(-20 * Time.deltaTime))), 0);
     }
-
-    Vector3 SmoothBodyRotation(Vector3 targetRotation)
+    
+    Vector3 RotateBodyAccordingToTop(float deg)
     {
-        float bodyRotationY = Mathf.LerpAngle(robotBody.eulerAngles.y, robotTop.eulerAngles.y, bodyRotationDamping * (1 - Mathf.Exp(-20 * Time.deltaTime)));
-       //1 bodyRotationY = AjustForVeryFastTopRotation(bodyRotationY, targetRotation); //temporarly commented due to unexplainable rotation bug
+        float bodyRotationY = Mathf.LerpAngle(robotBody.eulerAngles.y, robotTop.eulerAngles.y + deg, bodyRotationDamping * (1 - Mathf.Exp(-20 * Time.deltaTime)));
+        //1 bodyRotationY = AjustForVeryFastTopRotation(bodyRotationY, targetRotation); //temporarly commented due to unexplainable rotation bug
         return new Vector3(0, bodyRotationY, 0);
     }
 
     float AjustForVeryFastTopRotation(float bodyRotationY, Vector3 targetRotation)
     {
-       float y = Quaternion.Angle(robotTop.transform.rotation, robotBody.transform.rotation);
+        float y = Quaternion.Angle(robotTop.transform.rotation, robotBody.transform.rotation);
         if (y > 90)
         {
             Vector3 cross = Vector3.Cross(topPreviousRotation * Vector3.forward, this.robotTop.rotation * Vector3.forward);
@@ -103,9 +175,14 @@ public class TCameraController : MonoBehaviour
         }
         return y;
     }
-    void ShiftCamBehind()
+
+    void ShiftCamBehindTop()
     {
         Vector3 tempCam = robotTop.position - transform.forward.normalized * distFromTarget;
-        transform.position = new Vector3(tempCam.x, robotTop.position.y + distAboveTarget, tempCam.z);
+
+        float camDeltaX = this.transform.eulerAngles.x <= 30f ? 30f - this.transform.eulerAngles.x : 30f + 360f - this.transform.eulerAngles.x;
+        float aboveTarget = 8f - 8f / 50f * camDeltaX;
+
+        transform.position = new Vector3(tempCam.x, robot.position.y + aboveTarget, tempCam.z);
     }
 }
